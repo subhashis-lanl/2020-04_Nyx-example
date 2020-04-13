4043fe298574ff8854138752bc496d9e5c92636f
David Rogers <dhrogers@login3.summit.olcf.ornl.gov>
2020-04-13 15:41:16 -0400

    update

--- inputs/submit.sh
+++ inputs/submit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #BSUB -J <pantheon_workflow_jid> 
-#BSUB -nnodes 6
+#BSUB -nnodes 8
 #BSUB -P CSC340
-#BSUB -W 00:10
+#BSUB -W 00:07
 
 module load gcc/6.4.0
-module load cuda/10.1.168
+module load cuda/10.1.168 
 module load cmake/3.14.2
 module load hdf5/1.8.18
 
-jsrun -n 6 -g 1 <pantheon_workflow_dir>/sw4/optimize/sw4 hayward-att-h100.in
+jsrun -n 8 -g 1  <pantheon_run_dir>/Nyx3d.gnu.PROF.MPI.OMP.ex inputs
--- run/run.sh
+++ run/run.sh
@@ -12,13 +12,21 @@ fi
 
 mkdir $RUN_DIR
 
-cp env.sh $RUN_DIR
-cp -rf pantheon $RUN_DIR
+# copy input files
 cp inputs/ascent/* $RUN_DIR
-cp inputs/sw4/* $RUN_DIR
 cp inputs/* $RUN_DIR
+
+# copy executable
+cp $PANTHEON_WORKFLOW_DIR/nyx/Nyx/Exec/LyA/Nyx3d.gnu.PROF.MPI.ex $RUN_DIR
+
+# go to run dir and update the submit script
 pushd $RUN_DIR
+
 sed -i "s/<pantheon_workflow_jid>/${PANTHEON_WORKFLOW_JID}/" submit.sh
 sed -i "s#<pantheon_workflow_dir>#${PANTHEON_WORKFLOW_DIR}#" submit.sh
+sed -i "s#<pantheon_run_dir>#${RUN_DIR}#" submit.sh
+
+
+# submit
 bsub submit.sh
 
