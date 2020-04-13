1e417c437ec5b1f2263310e696b02d076a4464c0
David Rogers <dhrogers@login3.summit.olcf.ornl.gov>
2020-04-13 15:24:33 -0400

    updates to work with installed ascent

--- setup/install-app.sh
+++ setup/install-app.sh
@@ -17,11 +17,13 @@ git clone --branch ascent https://github.com/AMReX-Astro/Nyx.git
 pushd Nyx/Exec/LyA
 
 # static build on summit
-ASCENT_INSTALL_DIR=/gpfs/alpine/world-shared/csc340/software/ascent/current/summit/openmp/gnu/ascent-install/
+ASCENT_INSTALL_DIR=/gpfs/alpine/world-shared/csc340/software/ascent/current/summit/openmp/gnu/ascent-install
 
 make -j 4 \
         AMREX_HOME=${PANTHEON_WORKFLOW_DIR}/$PACKAGEDIR/amrex \
         USE_ASCENT_INSITU=TRUE \
-        ASCENT_DIR=$ASCENT_INSTALL_DIR
+        USE_MPI=TRUE \
+        USE_OMP=FALSE \
+        ASCENT_HOME=$ASCENT_INSTALL_DIR
 popd
 popd
