57f5012d5af23f58c307a9acf95d76636093c166
David Rogers <dhrogers@login1.summit.olcf.ornl.gov>
2020-04-24 10:44:42 -0400

    updating env

--- pantheon_env.sh
+++ pantheon_env.sh
@@ -1,22 +1,16 @@
-# Choose directory where "opt" will be created
-# "opt" is the home for all source and install directories
-# for all dependencies.
+# Pantheon environment file
 
 # read values from pantheon file
 export PANTHEON_WORKFLOW_ID=`awk '/pantheonid/{print $NF}' pantheon/pantheon.yml`
-# create the job id - a lower case version of the workflow id
+    # create the job id - a lower case version of the workflow id
 export PANTHEON_WORKFLOW_JID=`echo "$PANTHEON_WORKFLOW_ID" | awk '{print tolower($0)}'`
 export PANTHEON_APP=`awk '/workflow_app/{print $NF}' pantheon/pantheon.yml`
 
 # this instance's working directory
-export PANTHEON_BASE_PATH=$MEMBERWORK/csc340
+export PANTHEON_BASE_PATH=$MEMBERWORK/$SUMMIT_ALLOCATION
 export PANTHEONPATH=$PANTHEON_BASE_PATH/pantheon
 export PANTHEON_WORKFLOW_DIR=$PANTHEONPATH/$PANTHEON_WORKFLOW_ID
-
-# run directory
-PANTHEON_RUN_DIR=$PANTHEON_WORKFLOW_DIR/results
-
-# data directory
+export PANTHEON_RUN_DIR=$PANTHEON_WORKFLOW_DIR/results
 export PANTHEON_DATA_DIR=$PANTHEON_WORKFLOW_DIR/data
 
 # cinema
@@ -27,9 +21,11 @@ export CINEMA_PYTHONPATH=$(pwd)/submodules/cinema_lib/.local/summit/anaconda3/5.
 echo ----------------------------------------------------------------------
 echo Panthoen Environment
 echo ----------------------------------------------------------------------
+echo PANTHEONPATH.........: $PANTHEONPATH
+echo PANTHEON_WORKFLOW_DIR: $PANTHEON_WORKFLOW_DIR
+echo PANTHEON_RUN_DIR.....: $PANTHEON_RUN_DIR
+echo PANTHEON_DATA_DIR....: $PANTHEON_DATA_DIR
 echo PANTHEON_WORKFLOW_ID.: $PANTHEON_WORKFLOW_ID
 echo PANTHEON_WORKFLOW_JID: $PANTHEON_WORKFLOW_JID
 echo PANTHEON_APP.........: $PANTHEON_APP
-echo PANTHEON_WORKFLOW_DIR: $PANTHEON_WORKFLOW_DIR
-echo PANTHEON_RUN_DIR.....: $PANTHEON_RUN_DIR
 echo ----------------------------------------------------------------------
