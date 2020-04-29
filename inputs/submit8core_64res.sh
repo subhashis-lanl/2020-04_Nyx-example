aadd3b86207b86d40902dcb981e347e521ebd984
Ayan Biswas <ayanju04@gmail.com>
2020-04-29 12:15:55 -0400

    modifications for running 64, 128 resolutions

--- inputs/submit8core_64res.sh
+++ inputs/submit8core_64res.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #BSUB -J <pantheon_workflow_jid> 
-#BSUB -nnodes 1
+#BSUB -nnodes 8
 #BSUB -P AST160
-#BSUB -W 00:30
+#BSUB -W 00:03
 
 module load gcc/6.4.0
 module load cuda/10.1.168 
 module load cmake/3.14.2
 module load hdf5/1.8.18
 
-jsrun -n 1 -g 1  <pantheon_run_dir>/Nyx3d.gnu.PROF.MPI.ex inputs_cpu.128-32 max_step=10
+jsrun -n 8 -g 1  <pantheon_run_dir>/Nyx3d.gnu.PROF.MPI.ex inputs max_step=10
