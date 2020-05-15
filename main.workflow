bc9637f134b2a6815e3e5ca76b8ed29fc72a17e7
Soumya Dutta <dutta@batch4.summit.olcf.ornl.gov>
2020-05-14 21:26:58 -0400

    added commit# for Nyx and AMReX and turned OMP on for Nyx

--- main.workflow
+++ main.workflow
@@ -1,27 +1,9 @@
 
 workflow "ECP-Artifacts_002" {
-  resolves = "postprocess"
+  resolves = "install dependencies"
 }
 
 action "install dependencies" {
   uses = "sh"
   args = "./sbang.sh setup/install-deps.sh"
-}
-
-action "execute" {
-  needs = "install dependencies"
-  uses = "sh"
-  args = "./sbang.sh run/run.sh"
-}
-
-action "check job completion" {
-  needs = "execute"
-  uses = "sh"
-  args = "./sbang.sh run/wait_for_completion.sh"
-}
-
-action "postprocess" {
-  needs = "check job completion"
-  uses = "sh"
-  args = "./sbang.sh postprocess/postprocess.sh"
 }
--- setup/install-app.sh
+++ setup/install-app.sh
@@ -8,23 +8,39 @@ echo "--------------------------------------------------"
 echo "PTN: building $PACKAGE"
 echo "--------------------------------------------------"
 
+# commits
+NYX_COMMIT=bd7fe8a9f553b9588ea1f90b37705add05d9fec2
+AMREX_COMMIT=a595fc350b7c610799233b46f0f7aa0347d57404
+
 mkdir $PACKAGEDIR 
 pushd $PACKAGEDIR
 
+# Clone AMReX repository
 git clone --branch development https://github.com/AMReX-Codes/amrex.git
+pushd amrex
+git checkout $AMREX_COMMIT
+popd
+
+# Clone ASCENT repository
 git clone --branch ascent https://github.com/AMReX-Astro/Nyx.git
+pushd Nyx
+git checkout $NYX_COMMIT
+popd
+
 
 pushd Nyx/Exec/LyA
 
 # static build on summit
 ASCENT_VERSION=0.5.2-pre
-ASCENT_INSTALL_DIR=/gpfs/alpine/world-shared/$SUMMIT_ALLOCATION/software/ascent/$ASCENT_VERSION/summit/openmp/gnu/ascent-install
+ASCENT_INSTALL_DIR=/gpfs/alpine/world-shared/csc340/software/ascent/$ASCENT_VERSION/summit/openmp/gnu/ascent-install
+#ASCENT_INSTALL_DIR=ASCENT_INSTALL_DIR=/gpfs/alpine/proj-shared/csc340/larsen/ascent_5_5_20_openmp/uberenv_libs/ascent-install
 
-make -j 4 \
+
+make -j \
         AMREX_HOME=$PANTHEON_WORKFLOW_DIR/$PACKAGEDIR/amrex \
         USE_ASCENT_INSITU=TRUE \
         USE_MPI=TRUE \
-        USE_OMP=FALSE \
+        USE_OMP=TRUE \
         ASCENT_HOME=$ASCENT_INSTALL_DIR
 popd
 popd
